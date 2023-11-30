21a6b1dea8683514edb63202bcca2fbe31b81894
Akshat Sabharwal <akshatsabharwal35@gmail.com>
2023-11-30 22:01:48 +0530

    Added Footer component to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Footer } from "./pages/home/components/Footer";
 import { Home } from "./pages/home/main";
 
 const App = () => {
   return (
     <>
       <Home />
+      <Footer />
     </>
   );
 };
