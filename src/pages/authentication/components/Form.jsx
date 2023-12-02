d8115c4d4f9057a5725d1143541ae0ce46d340fb
Akshat Sabharwal <akshatsabharwal35@gmail.com>
2023-12-02 11:00:56 +0530

    Authentication page created and routing added to it

--- src/pages/authentication/components/Form.jsx
+++ src/pages/authentication/components/Form.jsx
@@ -1,3 +1,21 @@
+import authForm from "../../../assets/form.png";
+
 export const Form = () => {
-  return <h1>Page under construction...</h1>;
+  return (
+    <section className="auth">
+      <aside id="left">
+        <h1>Form</h1>
+        <img src={authForm} alt="Authentication Form" />
+      </aside>
+      <aside id="right">
+        <form>
+          <fieldset>
+            <input type="text" placeholder="Username" id="username" />
+            <input type="password" placeholder="Password" id="password" />
+          </fieldset>
+          <input type="submit" id="submit" />
+        </form>
+      </aside>
+    </section>
+  );
 };
